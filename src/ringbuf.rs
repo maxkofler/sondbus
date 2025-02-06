50b431c9dedc7f4e2458dba390acb2f43591a4c6
Max Kofler <max@acacialinux.org>
2025-02-06 21:22:21 +0100

    fixup! Add a Ringbuffer implementation

--- src/ringbuf.rs
+++ src/ringbuf.rs
@@ -1,20 +1,29 @@
 use core::sync::atomic::{compiler_fence, Ordering};
 
+/// A lock-free ring buffer
 pub struct RingBuffer<T, const S: usize> {
-    buf: [Option<T>; S],
+    buf: [T; S],
     read: usize,
     write: usize,
 }
 
 impl<T, const S: usize> RingBuffer<T, S> {
-    pub fn new() -> Self {
+    /// Create a new ring buffer
+    /// # Arguments
+    /// * `buf` - The underlying buffer of data to wrap
+    pub fn new(buf: [T; S]) -> Self {
         Self {
-            buf: [const { None }; S],
+            buf,
             read: 0,
             write: 0,
         }
     }
 
+    /// Push a new element into the ring buffer
+    /// # Arguments
+    /// * `data` - The element to push
+    /// # Returns
+    /// Ok(), else the element that failed to insert
     pub fn push(&mut self, data: T) -> Result<(), T> {
         compiler_fence(Ordering::SeqCst);
         let write = self.write;
@@ -27,7 +36,7 @@ impl<T, const S: usize> RingBuffer<T, S> {
             return Err(data);
         }
 
-        self.buf[write] = Some(data);
+        self.buf[write] = data;
 
         compiler_fence(Ordering::SeqCst);
         self.write = next_write;
@@ -36,7 +45,10 @@ impl<T, const S: usize> RingBuffer<T, S> {
         Ok(())
     }
 
-    pub fn pop(&mut self) -> Option<T> {
+    /// Retrieves one element from the ring buffer
+    /// # Returns
+    /// The element, if available, otherwise `None`
+    pub fn pop(&mut self) -> Option<&T> {
         compiler_fence(Ordering::SeqCst);
         let read = self.read;
         compiler_fence(Ordering::SeqCst);
@@ -47,14 +59,14 @@ impl<T, const S: usize> RingBuffer<T, S> {
             return None;
         }
 
-        let data = self.buf[self.read].take();
-        let read = Self::next(read); // read.overflowing_add(1).0;
+        let data = &self.buf[self.read];
+        let read = Self::next(read);
 
         compiler_fence(Ordering::SeqCst);
         self.read = read;
         compiler_fence(Ordering::SeqCst);
 
-        data
+        Some(data)
     }
 
     pub fn next(i: usize) -> usize {
