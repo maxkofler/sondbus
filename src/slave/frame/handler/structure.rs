ec710f942eae89f9a4f84944850b23b109c8159a
Max Kofler <max@acacialinux.org>
2025-02-28 11:25:27 +0100

    Change StructHandler to OwnedStructHandler

--- src/slave/frame/handler/structure.rs
+++ src/slave/frame/handler/structure.rs
@@ -1,7 +1,7 @@
 use crate::crc8::{CRC8Autosar, CRC};
 
 /// A handler to pull in a structure from the bus
-pub struct StructHandler<T> {
+pub struct OwnedStructHandler<T> {
     crc: CRC8Autosar,
     data: T,
     pos: usize,
@@ -9,14 +9,14 @@ pub struct StructHandler<T> {
 
 /// The result from the `handle()` function to
 /// dictate what to do next
-pub enum StructHandlerResult<T> {
+pub enum OwnedStructHandlerResult<T> {
     /// Continue with this handler - it is not yet done
-    Continue(StructHandler<T>),
+    Continue(OwnedStructHandler<T>),
     /// This handler is done and the data is ready
     Done(T),
 }
 
-impl<T> StructHandler<T> {
+impl<T> OwnedStructHandler<T> {
     /// Construct a new handler
     /// # Arguments
     /// * `crc` - The CRC to populate
@@ -28,7 +28,7 @@ impl<T> StructHandler<T> {
     /// Handles an incoming byte
     /// # Arguments
     /// * `data` - The byte to process
-    pub fn handle(mut self, data: u8) -> StructHandlerResult<T> {
+    pub fn handle(mut self, data: u8) -> OwnedStructHandlerResult<T> {
         self.crc.update_single(data);
         let ptr: *mut u8 = unsafe { core::mem::transmute(&mut self.data) };
         unsafe { *ptr = data };
@@ -36,9 +36,9 @@ impl<T> StructHandler<T> {
         self.pos += 1;
 
         if self.pos >= size_of::<T>() {
-            StructHandlerResult::Done(self.data)
+            OwnedStructHandlerResult::Done(self.data)
         } else {
-            StructHandlerResult::Continue(self)
+            OwnedStructHandlerResult::Continue(self)
         }
     }
 }
