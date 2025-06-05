91f7122c378b9f11f3ac621a66aba71a37757ed0
Max Kofler <max@acacialinux.org>
2025-06-05 18:26:23 +0200

    slave: Remove state machines for single big state machine impl

--- src/slave.rs
+++ src/slave.rs
@@ -1,14 +1,4 @@
-mod request;
 use replace_with::replace_with_or_abort;
-pub use request::*;
-
-mod response;
-pub use response::*;
-
-use crate::{
-    crc8::{CRC8Autosar, CRC},
-    SINGLE_START_BYTE,
-};
 
 #[derive(Debug, Default)]
 pub struct SlaveHandle {
@@ -25,9 +15,6 @@ pub struct SlaveCore {
 pub enum BusState {
     #[default]
     Idle,
-    Request(RequestState),
-    ResponseStart(ResponseState),
-    Response(ResponseState),
 }
 
 impl SlaveHandle {
@@ -72,39 +59,7 @@ impl BusState {
             // If we receive anything other than the start byte, we might
             // be out of sync with the bus and disable the `in_sync` flag
             //
-            Self::Idle => (
-                if data == SINGLE_START_BYTE {
-                    Self::Request(RequestState::Command(
-                        CRC8Autosar::new().update_single_move(data),
-                    ))
-                } else {
-                    core.in_sync = false;
-                    Self::Idle
-                },
-                None,
-            ),
-
-            //
-            // If we are an the request handling state, forward all
-            // bytes to the request state RX handler to proceed
-            //
-            Self::Request(r) => r.rx(data, core),
-
-            //
-            // All other states are errors in the sense that a RX is not handled,
-            // but the `in_sync` flag will be disabled, as a received byte in
-            // these states is not expected and thus not allowed and would hint
-            // at a bus timing or conformance violation. This will transition
-            // the bus state back to Idle to wait for a new sync.
-            //
-            Self::ResponseStart(_) => {
-                core.in_sync = false;
-                (Self::Idle, None)
-            }
-            Self::Response(_) => {
-                core.in_sync = false;
-                (Self::Idle, None)
-            }
+            Self::Idle => (Self::Idle, None),
         }
     }
 
@@ -116,45 +71,4 @@ impl BusState {
 }
 
 #[cfg(test)]
-mod tests {
-    use crate::{
-        crc8::{CRC8Autosar, CRC},
-        slave::{BusState, RequestState, SlaveCore, SlaveHandle},
-        SINGLE_START_BYTE,
-    };
-
-    /// Tests that the slave correctly handles an incoming start byte
-    #[test]
-    fn rx_single_start_byte() {
-        let mut core = SlaveCore { in_sync: false };
-        let state = BusState::Idle.rx(SINGLE_START_BYTE, &mut core);
-        assert_eq!(
-            state.0,
-            BusState::Request(RequestState::Command(
-                CRC8Autosar::new().update_single_move(SINGLE_START_BYTE)
-            )),
-            "Single-Command start byte"
-        )
-    }
-
-    #[test]
-    fn rx_nop() {
-        let mut handle = SlaveHandle::default();
-        let mut crc = CRC8Autosar::new();
-
-        handle.rx(SINGLE_START_BYTE);
-        crc.update_single(SINGLE_START_BYTE);
-
-        handle.rx(0x00);
-        crc.update_single(0x00);
-
-        assert_eq!(
-            handle.state,
-            BusState::Request(RequestState::CRC(None, crc.clone().finalize()))
-        );
-
-        handle.rx(crc.finalize());
-
-        assert_eq!(handle.state, BusState::Idle);
-    }
-}
+mod tests {}
