f320adc8cc57e4cf87af19bb22c446b41d63b54e
Max Kofler <max@acacialinux.org>
2025-06-08 22:14:40 +0200

    Slave: Implement WaitForCommandState

--- src/slave.rs
+++ src/slave.rs
@@ -1,5 +1,11 @@
 use replace_with::replace_with_or_abort;
 
+use crate::{
+    command::Command,
+    crc8::{CRC8Autosar, CRC},
+    SINGLE_START_BYTE,
+};
+
 #[derive(Debug, Default)]
 pub struct SlaveHandle {
     state: BusState,
@@ -13,8 +19,12 @@ pub struct SlaveCore {
 
 #[derive(PartialEq, Debug, Default)]
 pub enum BusState {
+    /// The idle bus state that waits for a start byte
     #[default]
     Idle,
+
+    /// Wait for the command byte and parse it
+    WaitForCommand(CRC8Autosar),
 }
 
 impl SlaveHandle {
@@ -59,7 +69,30 @@ impl BusState {
             // If we receive anything other than the start byte, we might
             // be out of sync with the bus and disable the `in_sync` flag
             //
-            Self::Idle => (Self::Idle, None),
+            Self::Idle => (
+                if data == SINGLE_START_BYTE {
+                    Self::WaitForCommand(CRC8Autosar::new().update_single_move(data))
+                } else {
+                    Self::sync_lost(core)
+                },
+                None,
+            ),
+
+            //
+            // Wait for the command byte to be received
+            // and parse it. If that fails (unknown command)
+            // we'll return to the Idle state
+            //
+            Self::WaitForCommand(crc) => match Command::from_u8(data) {
+                Some(cmd) => {
+                    let _crc = crc.update_single_move(data);
+                    match cmd {
+                        Command::NOP => (Self::Idle, None),
+                        _ => panic!("Unimplemented command"),
+                    }
+                }
+                None => (Self::sync_lost(core), None),
+            },
         }
     }
 
