507cd3e8bf610fadc2ccf147676d8aeb50a13f99
Max Kofler <max@acacialinux.org>
2025-09-27 12:37:42 +0200

    Start new implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,6 @@
 #![no_std]
+
+use crate::crc8::CRC8Autosar;
+
+pub mod crc8;
+pub mod slave;
---
+++ src/slave/transceiver.rs
@@ -0,0 +1,43 @@
+//! The transceiver implements the lowest layer of the sondbus communication protocol
+//! and handles synchronization of the communication and memory access.
+
+use crate::crc8::CRC8Autosar;
+
+type StateFunction = fn(&mut TransceiverContext, rx: Option<u8>) -> Option<u8>;
+const STATES: [StateFunction; 1] = [wait_for_start];
+
+#[repr(usize)]
+#[derive(Clone)]
+enum State {
+    WaitForStart = 0,
+    WaitForCommand = 1,
+}
+
+pub struct TransceiverContext {
+    state: State,
+    crc: CRC8Autosar,
+}
+
+impl TransceiverContext {
+    pub const fn new() -> Self {
+        Self {
+            state: State::WaitForStart,
+            crc: CRC8Autosar::new_const(),
+        }
+    }
+
+    pub fn handle(&mut self, rx: Option<u8>) -> Option<u8> {
+        let state = self.state.clone() as usize;
+        STATES[state as usize](self, rx)
+    }
+}
+
+fn wait_for_start(ctx: &mut TransceiverContext, rx: Option<u8>) -> Option<u8> {
+    if let Some(rx) = rx {
+        if rx == 0x55 {
+            ctx.state = State::WaitForCommand;
+        }
+    }
+
+    None
+}
