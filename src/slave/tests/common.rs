a891b29a6df7e6e0484a0f48c6634c05a06d1874
Max Kofler <max@acacialinux.org>
2025-09-27 11:31:44 +0200

    Restart implementation

--- src/slave/tests/common.rs
+++
@@ -1,9 +0,0 @@
-//! Common utility functions used for testing the slave
-
-use crate::slave::CallbackAction;
-
-/// A callback that panics if called, informing that the callback
-/// should never be called
-pub fn rx_callback_panic(_: CallbackAction) -> bool {
-    panic!("Callback was called when not allowed");
-}
